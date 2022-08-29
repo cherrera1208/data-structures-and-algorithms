b502c470dd8e2733099e6995ba08d5691a2bc8a1
Carlos A. Herrera <cherrera1208@gmail.com>
2022-08-28 21:55:56 -0700

    test 5 & 6 pass, challenge complete

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -117,7 +117,7 @@ let starWarsData = [{
 
 const returnNames = (arr) => {
   // Solution code here...
-  arr.reduce((accArr, curr) => {
+  return arr.reduce((accArr, curr) => {
     accArr.push(curr.name);
     return accArr;
   }, []);
@@ -133,6 +133,7 @@ Note: You must use reduce for this challenge. You may not use the built-in .reve
 
 const reversedString = (str) => {
   // Solution code here...
+  return str.split('').reduce((a, b) => b + a);
 };
 
 /* ------------------------------------------------------------------------------------------------
